74d88df684cbab2ffc96c85e4e16a0c21e879618
Eruzax <coolbean1123@yahoo.com>
2024-02-06 21:51:20 -0500

    Get comment text

--- main.py
+++ main.py
@@ -13,10 +13,46 @@ import reddit_api
 # upvote count ups
 # downvote count downs
 # upvote ratio upvote_ratio
-# picture to text?
+# picture to text?  
 
-comments = reddit_api.get_comments("1ah5s7j")
-print(comments.json())
+comments = reddit_api.get_comments("1akcxex")
+comments_str = comments.json()
+
+# print(comments_str[0].get('data'))
+# print(comments_str[0].get('data').keys())
+# print(comments_str[0].get('data').get('children'))
+# print(type(comments_str[0].get('data').get('children')))
+# print("Break")
+# print(comments_str[0].get('data').get('children'))
+# print(type(comments_str[0].get('data').get('children')[0]))
+# print(comments_str[0].get('data').get('children')[0].keys())
+#
+# print(comments_str[0].get('data').get('children')[0].get('data'))
+# print(type(comments_str[0].get('data').get('children')[0].get('data')))
+#
+# print(comments_str[0].get('data').get('children')[0].get('data').keys())
+
+print("Post Text:")
+print(comments_str[0].get('data').get('children')[0].get('data').get('selftext'))
+
+# print(type(comments_str[1]))
+# print(comments_str[1].keys())
+# print(comments_str[1].get('data'))
+# print(type(comments_str[1].get('data')))
+# print(comments_str[1].get('data').keys())
+# print(comments_str[1].get('data').get('children'))
+# print(type(comments_str[1].get('data').get('children')))
+# print(type(comments_str[1].get('data').get('children')[0]))
+# print(comments_str[1].get('data').get('children')[0].keys())
+# print(comments_str[1].get('data').get('children')[0].get('data'))
+# print(type(comments_str[1].get('data').get('children')[0].get('data')))
+# print(comments_str[1].get('data').get('children')[0].get('data').keys())
+
+print("Comments:")
+# print(comments_str[1].get('data').get('children')[0].get('data').get('body'))
+for i in range(1, len(comments_str[1].get('data').get('children'))):
+    print(comments_str[1].get('data').get('children')[i].get('data').get('body'))
+
 
 # response, chat_history = gpt_api.get_response("What is RCOS")
 
