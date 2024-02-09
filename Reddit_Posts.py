d787089715f028c32afd6ac411b2942891aef09c
Colin Lin <leyradc@gmail.com>
2024-02-09 18:30:03 -0500

    Redesign Reddit_Posts.py
    Signed-off-by: Colin Lin <leyradc@gmail.com>

--- Reddit_Posts.py
+++ Reddit_Posts.py
@@ -1,77 +1,92 @@
 import praw
 
-class Reddit_Comment:
-    def __init__(self):
-        self.author=""
-        self.ups=""
-        self.downs=""
-        self.content=""
-    
-    def print_comments(self):
-        print(self.author)
+ID = "mGJKXOitGGulU5pBJ9Zmqg"
+SECRIT_KEY = "zZR3V_O4kRdzjJqKZN9-oNluADiHfg"
+USER_NAME = "WallStreetPulse"
+PASSWORD = "WSPdevteam"
 
-class Reddit_Post:
-    def __init__(self):
-        self.author=""
-        self.title=""
-        self.content=""
-        self.image_urls=[]
-        self.ups=""
-        self.downs=""
-        self.comments=[]
-        self.replies=[]
-    
-    def print_post(self):
-        print(self.title)
-    
+reddit = praw.Reddit(
+    client_id=ID,
+    client_secret=SECRIT_KEY,
+    username=USER_NAME,
+    password=PASSWORD,
+    user_agent="testscript by u/fakebot3",
+)
+
 class Reddit_Posts:
-    def __init__(self):
-        self.posts=[]
-    
-    def __init__(self,num_posts=10,subreddit_name="wallstreetbets"):
-        self.posts=[]
+    def __init__(self, num_posts=10, subreddit_name="wallstreetbets"):
+        posts = reddit.subreddit(subreddit_name).hot(limit=num_posts)
+        # !!!!!self.posts sorting by score takes long time
+        print("Blocking on Generating posts")
+        # unsorted hotpost
+        self.posts = list(posts)
+        # sorted hotpost
+        # self.posts = sorted(posts, key=lambda x: x.score, reverse=True)
+        print("Release")
 
-        reddit=self.authenticate()
-        
-        top_posts = reddit.subreddit(subreddit_name).hot(limit=num_posts)
-        top_posts= sorted(top_posts, key=lambda x: x.score, reverse=True)
-        
-        for submission in top_posts:
-            p = Reddit_Post()
-            p.author=submission.author
-            p.title=submission.title
-            p.content=submission.selftext
-            p.image_urls=submission.url.endswith(('.jpg', '.jpeg', '.png', '.gif'))
-            p.ups=submission.ups
-            p.downs=submission.downs
-            
-            c = Reddit_Comment()
-            submission.comments.replace_more(limit=0)
-            top_comments = sorted(submission.comments.list(), key=lambda x: x.score, reverse=True)[:5]
+    ### Specification ###
+    # inputs:
+    #   n: the number of the post
+    # return: the author name of the n-th post
+    # String
+    def get_author(self, n):
+        return self.posts[n].author
 
-            for comment in top_comments:
-                c.author=comment.author
-                c.ups = comment.score
-                c.downs = comment.downs
-                c.content = comment.body
+    ### Specification ###
+    # inputs:
+    #   n: the number of the post
+    # return: the title of the n-th post
+    # String
+    def get_title(self, n):
+        return self.posts[n].title
 
-                p.comments.append(c)
+    ### Specification ###
+    # inputs:
+    #   n: the number of the post
+    # return: the content of the n-th post (What the author says)
+    # String
+    def get_content(self, n):
+        return self.posts[n].selftext
 
-            self.posts.append(p)
-        
+    ### Specification ###
+    # inputs:
+    #   n: the number of the post
+    # return: the content of the n-th post (What the author says)
+    # List of String.   ["urls","urls"]
+    def get_images(self, n):
+        return self.posts[n].url.endswith(('.jpg', '.jpeg', '.png', '.gif'))
+
+    ### Specification ###
+    # inputs:
+    #   n: the number of the post
+    # return: the upvotes of the n-th post
+    # String
+    def get_upvotes(self, n):
+        return self.posts[n].ups
+
+    ### Specification ###
+    # inputs:
+    #   n: the number of the post
+    # return: the downvotes of the n-th post
+    # String
+    def get_downvotes(self, n):
+        return self.posts[n].downs
+
+    ### Specification ###
+    # inputs:
+    #   n: the number of the post
+    #   num_comments: number of top comments you want under that post
+    # return: the comments of the n-th post
+    # List of Comments Object
+    #
+    # These are elements inside comment (could be more)
+    # comment.author
+    # comment.body
+    # comment.score
+    # comment.downs
+    def get_comments(self, n, num_comments):
+        self.posts[n].comments.replace_more(limit=0)
+        # !!!!!self.posts sorting by score takes long time
+        return sorted(self.posts[n].comments.list(), key=lambda x: x.score, reverse=True)[:num_comments]
 
 
-    def authenticate(self):
-        ID = "mGJKXOitGGulU5pBJ9Zmqg"
-        SECRIT_KEY = "zZR3V_O4kRdzjJqKZN9-oNluADiHfg"
-        USER_NAME = "WallStreetPulse"
-        PASSWORD = "WSPdevteam"
-
-        reddit = praw.Reddit(
-            client_id=ID,
-            client_secret=SECRIT_KEY,
-            username=USER_NAME,
-            password=PASSWORD,
-            user_agent="testscript by u/fakebot3",
-        )
-        return reddit
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from Reddit_Posts import Reddit_Posts
+from test import Reddit_Posts
 
 def main():
+    posts = Reddit_Posts(num_posts=10, subreddit_name="wallstreetbets")
+    print(f"Title: {posts.get_title(1)}")
+    print(f"Author: {posts.get_author(1)}")
+    print(posts.get_content(1))
 
-    posts = Reddit_Posts(num_posts=10,subreddit_name="wallstreetbets")
-    posts.posts[0].print_post()
+    print(f"First hot comment: {posts.get_comments(1, 2)[0].body}")
 
 
 
