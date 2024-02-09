c886ac9e0aaaf2fdeda14f62476f5f6ceacd1858
Colin Lin <leyradc@gmail.com>
2024-02-09 18:38:26 -0500

    Recover deleted sample usages
    Signed-off-by: Colin Lin <leyradc@gmail.com>

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+'''
+import reddit_api
+import gpt_api
+
+
+# Sample usages
+posts = reddit_api.get_hot_posts()
+# print(posts.json())
+
+
+response, chat_history = gpt_api.get_response("What is RCOS")
+
+response, chat_history = gpt_api.get_response("What is WallStreetPulse")
+'''
+
+
 from Reddit_Posts import Reddit_Posts
 
 def main():
