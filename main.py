d23c8b6f083710fbd33bc20f78b30908a125170f
LordBowlingBall <108240517+LordBowlingBall@users.noreply.github.com>
2024-02-11 02:21:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,50 @@
-'''
-import reddit_api
-import gpt_api
+#import reddit_api
+#import gpt_api
 
 
 # Sample usages
-posts = reddit_api.get_hot_posts()
+#posts = reddit_api.get_hot_posts()
 # print(posts.json())
 
 
-response, chat_history = gpt_api.get_response("What is RCOS")
+#response, chat_history = gpt_api.get_response("What is RCOS")
 
-response, chat_history = gpt_api.get_response("What is WallStreetPulse")
-'''
+#response, chat_history = gpt_api.get_response("What is WallStreetPulse")
 
 
-from Reddit_Posts import Reddit_Posts
+import praw
+
+ID = "mGJKXOitGGulU5pBJ9Zmqg"
+SECRIT_KEY = "zZR3V_O4kRdzjJqKZN9-oNluADiHfg"
+USER_NAME = "WallStreetPulse"
+PASSWORD = "WSPdevteam"
+
+
+reddit = praw.Reddit(
+    client_id=ID,
+    client_secret=SECRIT_KEY,
+    username=USER_NAME,
+    password=PASSWORD,
+    user_agent="testscript by u/fakebot3",
+)
+def printPost(submission):
+    print("Title: " + submission.title)
+    print("Score: " + submission.score)
+    print("Body: " + submission.selftext)
+    print("Image URL: " + submission.url)
 
 def main():
-    posts = Reddit_Posts(num_posts=10, subreddit_name="wallstreetbets")
-    print(f"Title: {posts.get_title(1)}")
-    print(f"Author: {posts.get_author(1)}")
-    print(posts.get_content(1))
-
-    print(f"First hot comment: {posts.get_comments(1, 2)[0].body}")
+    for submission in reddit.subreddit("WallStreetbets").hot(limit=12):
+        while(submission.stickied == False):
+            continue
+        printPost(submission)
 
 
+#https://praw.readthedocs.io/en/stable/code_overview/models/subreddit.html    #praw.models.Subreddit
 
-if __name__ == "__main__":
-    main()
+
+
+
+
+
+
