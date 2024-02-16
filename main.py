ab46fbf72f827ecd363a4ef1cf78b93261d8a187
Eruzax <coolbean1123@yahoo.com>
2024-02-16 17:58:09 -0500

    Made a class for api functions

--- main.py
+++ main.py
@@ -1,62 +1,10 @@
-import reddit_api
+from reddit_api import Reddit_API
 # import gpt_api
 
+# Reddit API object
+api = Reddit_API()
+post = api.get_post_data("1asbggq")
 
-# Sample usages
-# posts = reddit_api.get_hot_posts()
-# print(posts.json())
-
-
-# things to get
-# title of the post
-# comments
-# upvote count ups
-# downvote count downs
-# upvote ratio upvote_ratio
-# picture to text?  
-
-comments = reddit_api.get_post_data("1apwf44")
-comments_list = comments.json()
-# print(comments_str[0].get('data'))
-# print(comments_str[0].get('data').keys())
-# print(comments_str[0].get('data').get('children'))
-# print(type(comments_str[0].get('data').get('children')))
-# print("Break")
-# print(comments_str[0].get('data').get('children'))
-# print(type(comments_str[0].get('data').get('children')[0]))
-# print(comments_str[0].get('data').get('children')[0].keys())
-#
-# print(comments_str[0].get('data').get('children')[0].get('data'))
-# print(type(comments_str[0].get('data').get('children')[0].get('data')))
-#
-# print(comments_str[0].get('data').get('children')[0].get('data').keys())
-
-# print("Post Text:")
-# print(comments_str[0].get('data').get('children')[0].get('data').get('selftext'))
-
-# Direct comments to posts
-# print(type(comments_str[1]))
-# print(comments_str[1].keys())
-# print(comments_str[1].get('data'))
-# print(type(comments_str[1].get('data')))
-# print(comments_str[1].get('data').keys())
-# print(comments_str[1].get('data').get('children'))
-# print(type(comments_str[1].get('data').get('children')))
-# print(type(comments_str[1].get('data').get('children')[0]))
-# print(comments_str[1].get('data').get('children')[0].keys())
-# print(comments_str[1].get('data').get('children')[0].get('data'))
-# print(type(comments_str[1].get('data').get('children')[0].get('data')))
-reddit_api.get_comments(comments_list[1])
-
-# Getting the replies of the
-# parent_comment = comments_list[1].get('data').get('children')[1].get('data')
-# print(comments_list[1].get('data').get('children')[1].get('data').keys())
-# # print(comments_list[1].get('data').get('children')[0].get('data').get('replies'))
-# # print(type(comments_list[1].get('data').get('children')[0].get('data').get('replies')))
-# # print(type(comments_list[1].get('data').get('children')[1].get('data').get('replies')))
-# # print(comments_list[1].get('data').get('children')[1].get('data').get('replies').keys())
-# # print(comments_list[1].get('data').get('children')[1].get('data').get('replies').get('data'))
-# print(parent_comment.get('replies').get('data').keys())
-# print(parent_comment.get('replies').get('data').get('children'))
-# print(parent_comment.get('replies').get('data').get('children')[0].get('data'))
-# print(parent_comment.get('replies').get('data').get('children')[0].get('data').get('body'))
+# Current Issue:
+# I am unable to get replies under the more replies dropdown menu
+# can be solved using /api/morechildren
--- reddit_api.py
+++ reddit_api.py
@@ -1,14 +1,12 @@
 import requests
 # Setting up authorization header
 ID = "mGJKXOitGGulU5pBJ9Zmqg"
-SECRIT_KEY = "zZR3V_O4kRdzjJqKZN9-oNluADiHfg"
-auth = requests.auth.HTTPBasicAuth(ID, SECRIT_KEY)
+SECRET_KEY = "zZR3V_O4kRdzjJqKZN9-oNluADiHfg"
+auth = requests.auth.HTTPBasicAuth(ID, SECRET_KEY)
 
 with open('pswd.txt', 'r') as f:
     pswd = f.read()
 
-# https://www.reddit.com/prefs/apps
-# https://old.reddit.com/prefs/apps/
 data = {
     'grant_type': 'password',
     'username': 'WallStreetPulse',
@@ -18,28 +16,59 @@ data = {
 headers = {'User-Agent': "WallStreetPulse/0.0.1"}
 res = requests.post('https://www.reddit.com/api/v1/access_token', auth=auth, data=data, headers=headers)
 TOKEN = res.json()['access_token']
-
 headers['Authorization'] = f'bearer {TOKEN}'
-
-
-# Request using API
-# API can be found https://www.reddit.com/dev/api/
 BASE_URL = 'https://oauth.reddit.com/r/wallstreetbets'
 
-def get_hot_posts():
-    return requests.get( BASE_URL + "/hot", headers=headers)
 
-# ADD MORE USEFUL API HERE
-# Returns a list containing the data of the post
-def get_post_data(id):
-    return requests.get ( BASE_URL + "/comments/" + id, headers=headers)
+# things to get
+# title of the post
+# comments
+# upvote count ups
+# downvote count downs
+# upvote ratio upvote_ratio
+# picture to text?
+class Reddit_API:
+    def __init__(self):
+        self.client_id = ID
+        self.secret_key = SECRET_KEY
+        self.auth = auth
+        self.data = data
+        self.headers = headers
+        self.token = TOKEN
 
-def get_post_content(comments_list):
-    print(comments_list[0].get('data').get('children')[0].get('data').get('selftext'))
-def get_comments(comments_list):
-    # Prints the direct comments of the post
-    for i in range(0, len(comments_list.get('data').get('children'))):
-        print(comments_list.get('data').get('children')[i].get('data').get('body'))
+
+
+    # Gets hot posts
+    def get_hot_posts(self):
+        return requests.get(BASE_URL + "/hot", headers=headers).json()
+
+    # Returns a list containing the data of the post
+    def get_post_data(self, id):
+        return requests.get(BASE_URL + "/comments/" + id, headers=headers).json()
+
+    # Returns the content in the post
+    def get_post_content(self, comments_list):
+        print(comments_list[0].get('data').get('children')[0].get('data').get('selftext'))
+
+    # Gets all comments and all replies to a comment
+    def get_comments(self, comments_list):
+        # Prints the direct comments of the post
+        for i in range(0, len(comments_list.get('data').get('children'))):
+            print(comments_list.get('data').get('children')[i].get('data').get('body'))
+            # dict_keys(['count', 'name', 'id', 'parent_id', 'depth', 'children'])
+            print(comments_list.get('data').get('children')[i].get('data').get('depth'))
+            print(comments_list.get('data').get('children')[i].get('data').get('children'))
+
+            # Print all replies to the comment
+            if isinstance(comments_list.get('data').get('children')[i].get('data').get('replies'), dict):
+                self.get_comments(comments_list.get('data').get('children')[i].get('data').get('replies'))
+
+    # Gets a comment and all its replies
+    # comment should be in format post[1].get('data').get('children')[num]
+    def get_comment(self, comment):
+        # Prints the direct comment of the post
+        print(comment.get('data').get('body'))
+
         # Print the replies to the comment
-        if isinstance(comments_list.get('data').get('children')[i].get('data').get('replies'), dict):
-            get_comments(comments_list.get('data').get('children')[i].get('data').get('replies'))
+        if isinstance(comment.get('data').get('replies'), dict):
+            self.get_comments(comment.get('data').get('replies'))
