7bb66259b8eaa20cf10218e06134e5b642b9907c
LordBowlingBall <108240517+LordBowlingBall@users.noreply.github.com>
2024-02-18 05:15:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,34 +13,41 @@
 
 
 import praw
+import time
 
 ID = "mGJKXOitGGulU5pBJ9Zmqg"
 SECRIT_KEY = "zZR3V_O4kRdzjJqKZN9-oNluADiHfg"
-USER_NAME = "WallStreetPulse"
+user = "WallStreetPulse"
 PASSWORD = "WSPdevteam"
-
 
 reddit = praw.Reddit(
     client_id=ID,
     client_secret=SECRIT_KEY,
-    username=USER_NAME,
+    username=user,
     password=PASSWORD,
     user_agent="testscript by u/fakebot3",
 )
-def printPost(submission):
-    print("Title: " + submission.title)
-    print("Score: " + submission.score)
-    print("Body: " + submission.selftext)
-    print("Image URL: " + submission.url)
 
-def main():
-    for submission in reddit.subreddit("WallStreetbets").hot(limit=12):
-        while(submission.stickied == False):
-            continue
-        printPost(submission)
+def calculate_score(submission, num_followers):
+    weight_posts = 0.3
+    weight_followers = 0.2
+    weight_upvotes = 0.5
+
+    score = (weight_posts * submission.num_comments + weight_followers * num_followers + weight_upvotes * submission.score)
+    return score
+
+start_time = time.time() - 7*24*60*60
+
+for submission in reddit.subreddit("WallStreetbets").top(time_filter="week"):
+    if submission.stickied == False and submission.created_utc >= start_time:
+        num_followers = submission.author.followers
+        score = calculate_score(submission, num_followers)
+        print("\nTitle: ", submission.title)
+        print("Score: ", score)
 
 
 #https://praw.readthedocs.io/en/stable/code_overview/models/subreddit.html    #praw.models.Subreddit
+
 
 
 
