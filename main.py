bed15ec5fbe1ea2d31d09e5ff96179600d7d0ab2
Tiger Weng <wengj3@rpi.edu>
2024-02-19 23:56:44 -0500

    commited preprocessing function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-'''
+
 import reddit_api
 import gpt_api
 
@@ -7,22 +7,22 @@ import gpt_api
 posts = reddit_api.get_hot_posts()
 # print(posts.json())
 
-
-response, chat_history = gpt_api.get_response("What is RCOS")
-
-response, chat_history = gpt_api.get_response("What is WallStreetPulse")
-'''
+# response, chat_history = gpt_api.get_response("What is RCOS")
+#
+# response, chat_history = gpt_api.get_response("What is WallStreetPulse")
+#
 
 
 from Reddit_Posts import Reddit_Posts
 
 def main():
     posts = Reddit_Posts(num_posts=10, subreddit_name="wallstreetbets")
-    print(f"Title: {posts.get_title(1)}")
-    print(f"Author: {posts.get_author(1)}")
-    print(posts.get_content(1))
+    print(f"Title: {posts.get_title(3)}")
+    print(f"Author: {posts.get_author(3)}")
+    print(posts.get_content(3))
 
     print(f"First hot comment: {posts.get_comments(1, 2)[0]['content']}")
+
 
 
 
