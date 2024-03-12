aa44c569f06670ae19cfc036346d4b85693567f2
fastninja30 <spencerrsu@gmail.com>
2024-03-12 17:20:11 -0400

    Updated add_post function to accept more dynamic dictionaries and added a testing UI

--- SQLjsonHelper.py
+++ SQLjsonHelper.py
@@ -1,5 +1,6 @@
+from itertools import islice
 import sqlite3
-
+# https://docs.python.org/3/library/sqlite3.html
 conn = sqlite3.connect('postDB.db')
 
 cursor = conn.cursor()
@@ -15,7 +16,20 @@ input_dictionary = {
 
 
 def add_post():
-    cursor.execute("INSERT INTO redditPosts (postId, author, likes) VALUES (?, ?, ?)",
+    # Creates the sql prompt to insert a post
+    sql_prompt = "INSERT INTO redditPosts("
+    for key in islice(input_dictionary.keys(), 0, len(input_dictionary)-1, 1):
+        sql_prompt += key
+        sql_prompt += ", "
+    sql_prompt += list(input_dictionary.keys())[-1]
+    sql_prompt += ") VALUES ("
+    for x in range(len(input_dictionary)-1):
+        sql_prompt += "?, "
+    sql_prompt += "?)"
+    print("sql_prompt: ", sql_prompt)
+    print("\n")
+    # Executes prompt
+    cursor.execute(sql_prompt,
                    (input_dictionary["postId"], input_dictionary["author"], input_dictionary["likes"]))
     conn.commit()
 
@@ -28,15 +42,50 @@ def update_likes(new_likes):
 
 
 def display_table():
-    cursor.execute("SELECT * FROM redditPosts")
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
+    cursor.execute("SELECT COUNT(*) FROM redditPosts")
+    result = cursor.fetchall()
+    if result[0][0] == 0:
+        print("redditPosts is currently empty\n")
+
+    else:
+        cursor.execute("SELECT * FROM redditPosts")
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+        print("\n")
 
 
 def delete_post():
-    cursor.execute("DELETE FROM redditPosts WHERE id = ?", (input_dictionary["postId"],))
+    cursor.execute("DELETE FROM redditPosts WHERE postId = ?", (input_dictionary["postId"],))
     conn.commit()
 
+
+def clear_table():
+    cursor.execute("DELETE FROM redditPosts")
+
+
+def main():
+    action = "s"
+    while action != "q":
+        action = input("Enter an action:\n a - add_post \n d - delete post\n v - view table\n "
+                       "c - clear table\nq - quit\n "
+                       "----------------------\n")
+        print("Action: '", action, "'")
+        print("Output: ")
+        if action == "a":
+            add_post()
+        if action == "d":
+            delete_post()
+        if action == "v":
+            display_table()
+        if action == "c":
+            clear_table()
+        if action == "q":
+            print("Shutting down...")
+        print("-----------------------------------")
+
+
+if __name__ == "__main__":
+    main()
 
 conn.close()
